4b9ae294b9d635b40bb3449fda3a75f30bcb1d91
Apnea <3030770695@qq.com>
2026-03-28 22:45:55 +0800

    feat: 初步设计前端路由

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '',
+    name: 'Home',
+    component: () => import('@/pages/Home.vue'),
+    children: [
+      {
+        path: 'introduce',
+        name: 'Introduce',
+        component: () => import('@/pages/home/Introduce.vue')
+      },
+      {
+        path: 'myNovels',
+        name: 'MyNovels',
+        meta: { requireLogged: true },
+        component: () => import('@/pages/home/MyNovels.vue')
+      },
+      {
+        path: 'writing',
+        name: 'Writing',
+        meta: { requireLogged: true },
+        component: () => import('@/pages/home/Writing.vue')
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('@/pages/Auth.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
 
 export default router
