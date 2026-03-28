b496e053ba390b0c694def884a03cec70ca5d6ce
Apnea <3030770695@qq.com>
2026-03-28 22:36:29 +0800

    feat: 实现响应拦截器添加token、请求拦截器自动刷新

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosRequestConfig } from 'axios'
+import axios, { AxiosError, type AxiosRequestConfig } from 'axios'
+import useUserStore from './stores/userSotre'
+import router from '@/router/index.ts'
 
 // 实例化axios对象
 const request = axios.create({
@@ -12,6 +14,14 @@ const request = axios.create({
 // 配置请求拦截器
 request.interceptors.request.use(
     config => {
+        // 获取access token
+        const accessToken = localStorage.getItem('accessToken')
+
+        // 如果access token存在则添加到请求头
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`
+        }
+
         return config
     },
     error => {
@@ -24,7 +34,32 @@ request.interceptors.response.use(
     config => {
         return config
     },
-    error => {
+    async (error: AxiosError) => {
+        
+        // 获取响应状态码
+        const statusCode = error.response?.status
+
+        // 401错误自动进行重新验证
+        if (statusCode === 401) {
+
+            // 判断是否为刷新请求或已经重试
+            if (error.config?.url?.includes('auth/refresh')) {
+                router.replace('/auth')
+                return Promise.reject(error)
+            }
+
+            // 自动进行token刷新并重新发送请求
+            const userStore = useUserStore()
+            try {
+                await userStore.refresh()
+                return request(error.config!)
+            } catch(error) {
+                // 刷新失败则自动跳转到登录页面并返回错误
+                router.replace('/auth')
+                return Promise.reject(error)
+            }
+        }
+
         return Promise.reject(error)
     }
 )
